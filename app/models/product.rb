cc853acad17c7635b7f65382fd62f6b9170c1125
Harish Ramachandran <harishsr.coder@gmail.com>
2014-08-24 12:26:02 -0400

    C1: Creating the Catalog Listing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10, message: "is too short, sucka! 10 character minimum." }
   validates :image_url, allow_blank: true, format: {
     with:     %r{\.(gif|jpg|png)\Z}i,
     message:  'must be URL for GIF, PNG, or JPG image.' 
