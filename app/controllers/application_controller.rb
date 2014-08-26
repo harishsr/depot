c885066296cd154bd93c297583db34c19a1fc497
Harish Ramachandran <harishsr.coder@gmail.com>
2014-08-26 19:14:07 -0400

    D1: Finding a Cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :time_loaded
+
+  def time_loaded
+    return Time.new.strftime('%I:%M %p')
+  end 
 end
